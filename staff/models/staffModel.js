f184eaa9ebf1356ff75ab94ad49be67e016a9910
smilezdev <vixxjy@gmail.com>
2017-08-25 11:28:14 +0000

    updated the teacherSchema

--- staff/models/staffModel.js
+++ staff/models/staffModel.js
@@ -7,7 +7,15 @@ var Schema = mongoose.Schema;
 
 var TeacherSchema = new Schema({
     firstname: {type: String, required: true},
+    middlename: {type: String, required: true},
     lastname: {type: String, required: true},
+    gender: {type: String, required: true},
+    religion: {type: String, required: true},
+    bloodgroup: {type: String, required: true},
+    state: {type: String, required: true},
+    lga: {type: String, required: true},
+    date_of_birth: {type: String, required: true},
+    photo: {type: String, required: true},
     phone: {type: String, required: true},
     Created_date: { type: Date, default: Date.now }
 })
